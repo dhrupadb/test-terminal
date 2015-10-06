abc43cdd9d2f0a295a65070197453c33b3102f61
dhrupadb <dhrupadb@seas.upenn.edu>
2015-10-05 21:57:07 -0400

    Got LaTeX parsing working

--- js/latex-engine.js
+++ js/latex-engine.js
@@ -1,8 +1,13 @@
-function renderLatex() {
+function renderLatex(str) {
 	str = $("#input-text").val()
-	if (str.indexOf("$$") != -1) {
-		alert("Math Mode activated!");
+	out = ""
+	while (str.indexOf("$$") != -1) {
+		mbox = "\\mbox{"+str.substring(str.indexOf("$$"),-1)+" }";
+		str = str.substring(str.indexOf("$$")+2);
+		out = out + mbox + str.substring(str.indexOf("$$"),-1); 
+		str = str.substring(str.indexOf("$$")+2);
 	}
-	url = "http://latex.codecogs.com/gif.latex?"+str
-	$("#render-image").prop('src',url)
+	out = out + "\\mbox{"+str+" }";
+	url = "http://latex.codecogs.com/gif.latex?"+out;
+	$("#render-image").prop('src',url);
 }
